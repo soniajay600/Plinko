f2a2477ce7f47398633f4068c8e63a2f56e3c7e1
soniajay600 <77841871+soniajay600@users.noreply.github.com>
2021-06-05 21:18:42 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,22 @@
+class Particle{
+    constructor(x,y,r){
+      var options={
+
+        'restitution':0.4
+    }
+    this.body=Bodies.circle(x,y,r,options)
+    this.color=color(random(0,255),random(0,255),random(0,255))
+    World.add(world,this.body)
+    this.r=r;
+    
+}
+display(){
+    push()
+    translate(this.body.position.x,this.body.position.y)
+    rotate(this.body.angle)
+    fill(this.color)
+    ellipseMode(RADIUS)
+    ellipse(0,0,this.r,this.r)
+    pop();
+}
+}
